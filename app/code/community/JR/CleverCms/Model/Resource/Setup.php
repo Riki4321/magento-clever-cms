65a9ccb649589f333acc93107342f046c5976172
Johann Reinke <johann.reinke@gmail.com>
2012-02-17 16:53:58 +0100

    Enhanced module test

--- app/code/community/JR/CleverCms/Model/Resource/Setup.php
+++ app/code/community/JR/CleverCms/Model/Resource/Setup.php
@@ -4,7 +4,7 @@ class JR_CleverCms_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
 {
     /**
      * Do not install module if Magento is not installed yet.
-     * This prevents error when Mage_Cms data install.
+     * This prevents error during Mage_Cms data install.
      *
      * @see Mage_Core_Model_Resource_Setup::applyUpdates()
      */
@@ -12,8 +12,9 @@ class JR_CleverCms_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
     {
         if (!Mage::isInstalled()) {
             $modules = Mage::getConfig()->getNode('modules')->children();
+            $myModule = substr(__CLASS__, 0, strpos(__CLASS__, '_Model'));
             foreach ($modules as $moduleName => $moduleNode) {
-                if ($moduleName != 'JR_CleverCms') {
+                if ($moduleName != $myModule) {
                     Mage::getConfig()->addAllowedModules($moduleName);
                 }
             }
